9b829e9f6fbdb4d3857e4f2cef3f7462ad59b463
satoteharshal97 <37376689+satoteharshal97@users.noreply.github.com>
2022-01-08 11:51:20 +0530

    Added - Added types of medicine in list

--- src/MedicalShop/Main.java
+++ src/MedicalShop/Main.java
@@ -3,9 +3,19 @@ package MedicalShop;
 public class Main {
 
     public static void main(String[] args) {
-        Crocin crocin = new Crocin();
         MedicineStore medicineStore = new MedicineStore();
+
+        Crocin crocin = new Crocin();
+        Chawayanprash chawayanprash = new Chawayanprash();
+        Vicks vicks = new Vicks();
+        BTrim btrim = new BTrim();
+        Azithromycin azithromycin = new Azithromycin();
+
         medicineStore.add(crocin);
+        medicineStore.add(btrim);
+        medicineStore.add(chawayanprash);
+        medicineStore.add(vicks);
+        medicineStore.add(azithromycin);
     }
 
 }
